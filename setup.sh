e262b29ac759b6d2892dc8bd89e9e04da24fed6b
Aakarshan Singh <aakarshan221003@gmail.com>
2024-03-29 13:51:59 +0530

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 if [ "$1" = "stop" ];
 then
 	sudo docker compose down
+elif [ "$1" = "r" ]
+then
+	sudo docker stop $(sudo docker ps -a -q) 
+	sudo docker compose up -d
 else
 	sudo docker compose up -d
 fi
